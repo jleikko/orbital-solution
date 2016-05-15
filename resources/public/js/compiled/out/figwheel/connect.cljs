3a3aa962946927b8ab391fb752c26bc55ea765b8
Jaakko Leikko <jaakko@leikko.net>
2016-05-15 23:53:12 +0300

    minified

--- resources/public/js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [orbital-solution.core]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/orbital-solution.core.on-js-reload (apply js/orbital-solution.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'orbital-solution.core/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
